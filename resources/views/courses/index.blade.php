bc0b3f8e524306e032c6216f7b584d3bf091e77e
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-07-18 17:17:25 +0530

    Course Page Dev. 1

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -5,22 +5,46 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 	<title>Free Online Technology Tutorials by Dwij IT Solutions</title>
-	<meta name="description" content="We are publishing free online tutorials here for Student and enthusiastics." />
+	<meta name="description" content="{{ config('laraadmin.sitename') }} - {{ config('laraadmin.sitedesc') }}">
 	<meta name="keywords" content="Free, Tutorials, Python, Raspberry Pi, OpenCV" />
 	<meta name="author" content="Dwij IT Solutions" />
-	<link rel="shortcut icon" href="../dwij-it-solutions-favcon.png">
-	<link rel="stylesheet" type="text/css" href="css/normalize.css" />
-	<link rel="stylesheet" type="text/css" href="css/demo.css" />
-	<link rel="stylesheet" type="text/css" href="css/component.css" />
+
+	<meta property="og:title" content="{{ config('laraadmin.sitename') }}" />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="{{ config('laraadmin.sitename') }} - {{ config('laraadmin.sitedesc') }}" />
+
+	<link rel="shortcut icon" href="{{ asset('courses-assets/img/dwij-it-solutions-favcon.png') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('courses-assets/css/normalize.css') }}" />
+	<link rel="stylesheet" type="text/css" href="{{ asset('courses-assets/css/demo.css') }}" />
+	<link rel="stylesheet" type="text/css" href="{{ asset('courses-assets/css/component.css') }}" />
 </head>
 <body>
 	<div class="container">
 		<header>
-			<h1>Dwij IT Solutions <span>Free online tutorials for Students &amp; enthusiastics.</span></h1>  
+			<h1>Dwij IT Solutions <span>{{ config('laraadmin.sitedesc') }}</span></h1>  
 		</header>
 		<div class="wrapper">
 			<ul class="stage clearfix">
 
+				@foreach ($courses as $course)
+					<li class="scene">
+						<div class="movie" onclick="return true">
+							<div class="poster" style="background-image: url(../img/introduction-python-programming.jpg);"></div>
+							<div class="info">
+								<header>
+									<h1>{{ $course->name }}</h1>
+									<span class="rating">For Students</span>
+									<span class="duration">{{ $course->hours }} Hours</span>
+								</header>
+								<p>
+									{{ $course->description }}
+								</p>
+								<center><a class="toslides" href="http://dwij.net/tuts/python-basic">Python Basics</a></center>
+							</div>
+						</div>
+					</li>
+				@endforeach
+				
 				<li class="scene">
 					<div class="movie" onclick="return true">
 						<div class="poster"></div>
@@ -28,7 +52,7 @@
 							<header>
 								<h1>Introduction to Python Programming</h1>
 								<span class="rating">For Students</span>
-								<span class="duration">140 slides</span>
+								<span class="duration">20 Hours</span>
 							</header>
 							<p>
 								Python is a widely used general-purpose, high-level programming language that lets you work more quickly and integrate your systems more effectively. <br/><br/>
@@ -58,6 +82,11 @@
 				</li>
 
 			</ul>
+			<br>
+			<br>
+			<div class="footer-menu">
+				<a href="">{{ Auth::user()->name }}</a> - <a href="{{ url('/logout') }}">Logout</a>
+			</div>
 		</div><!-- /wrapper -->
 	</div><!-- /container -->
 <script>
