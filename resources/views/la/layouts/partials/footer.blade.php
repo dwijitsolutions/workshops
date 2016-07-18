1dc9faf2561c51871f6e26afb760e14bd19a1b0d
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-07-18 14:32:37 +0530

    footer powered

--- resources/views/la/layouts/partials/footer.blade.php
+++ resources/views/la/layouts/partials/footer.blade.php
@@ -1,7 +1,7 @@
 @if(!isset($no_padding))
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        Powered by <a href="http://dwijitsolutions.com">Dwij IT Solutions</a>
+        Powered by <a target="_blank" href="http://laraadmin.com">LaraAdmin</a> - Open source Admin Panel for Laravel by <a target="_blank" href="http://dwijitsolutions.com">Dwij IT Solutions</a>
     </div>
     <strong>Copyright &copy; 2016
 </footer>
